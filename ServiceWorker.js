4999fd9c29fe55209161c04830a97e2cfb3fe348
Heki-coder <victor@ines-thorsten.de>
2025-12-23 06:42:39 +0100

    game

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Christmas Run-1.0";
 const contentToCache = [
-    "Build/Christmas Run.loader.js",
-    "Build/Christmas Run.framework.js.gz",
-    "Build/Christmas Run.data.gz",
-    "Build/Christmas Run.wasm.gz",
+    "Build/4fb3b3c9e6a5b23775b03332c88676b7.loader.js",
+    "Build/96471e92190397fbd7033d6881327651.framework.js.unityweb",
+    "Build/217826925e06aa653d410198985c8b84.data.unityweb",
+    "Build/dfe956a7ba1bfc2b8b09bbb8c61c6b70.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
